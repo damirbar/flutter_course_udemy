8c5f7dbbb72e9decf372c6d09aa77056dff1bfe7
DESKTOP-D5S5QEJ\damir <damirbar@gmail.com>
2020-07-18 11:21:42 +0300

    until 1:54:24 - Added Container, RaisedButton (w/ an empty function), EdgeInsets margin

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,25 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('EasyList'),
         ),
-        body: Card(
-          child: Column(
-            children: <Widget>[
-              Image.asset('assets/banana.jpg'),
-              Text('Food Paradise')
-            ],
-          ),
-        ),
+        body: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                onPressed: () {},
+                child: Text('Add Product')
+              ),
+            ),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset('assets/banana.jpg'),
+                  Text('Bananas')
+                ],
+              ),
+            ),
+          ],
+        )
       ),
     );
   }
