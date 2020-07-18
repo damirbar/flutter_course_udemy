bece2e0b32d3c49b7ef08389c6e63a82dda84c83
DESKTOP-D5S5QEJ\damir <damirbar@gmail.com>
2020-07-18 17:50:47 +0300

    until 2:36:43 - added prints to see the initialization, build and update processes

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -3,18 +3,47 @@ import 'package:flutter/material.dart';
 import './products.dart';
 
 class ProductManager extends StatefulWidget {
+
+  final String startingProduct;
+
+  ProductManager(this.startingProduct) {
+    print('[ProductManager Widget] Constructor');
+  }
+
   @override
   State<StatefulWidget> createState() {
+    print('[ProductManager Widget] createState()');
     return _ProductManagerState();
   }
 
 }
 
 class _ProductManagerState extends State<ProductManager> {
-  List<String> _products = ['Food Tester']; 
+  List<String> _products = []; 
+
+  @override
+  void initState() {
+    print('[ProductManager State] initState()');
+
+    // "widget" is provided by the State object.
+    // It gives us access to the connected StatefulWidget and to
+    //   its properties.
+    _products.add(widget.startingProduct);
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(ProductManager oldWidget) {
+    // This will be executed whenever the connected widget receives
+    //   new external data.
+    // We get the old widget just in case we need something from there.
+    print('[ProductManager State] didUpdateWidget()');
+    super.didUpdateWidget(oldWidget);
+  }
 
   @override
   Widget build(BuildContext context) {
+    print('[ProductManager State] build()');
     return Column(children:
       [Container(
         margin: EdgeInsets.all(10.0),
--- lib/products.dart
+++ lib/products.dart
@@ -4,11 +4,13 @@ class Products extends StatelessWidget {
 
   final List<String> products;
   
-  Products(this.products);
+  Products(this.products) {
+    print('[Products Widget] Constructor');
+  }
 
   @override
   Widget build(BuildContext context) {
-
+    print('[Products Widget] build()');
     return Column(
       children:
         products.map((elem) => Card(
